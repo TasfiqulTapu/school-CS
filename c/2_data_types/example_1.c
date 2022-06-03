afdf7378130be6f57ed8277d653c896078389885
TasfiqulTapu <iamtasfiqultapu@gmail.com>
2022-06-03 20:07:08 +0600

    🤐 I suck at naming

---
+++ c/2_data_types/example_1.c
@@ -0,0 +1,9 @@
+// Use of data types
+// no newline character seriously?
+#include <stdio.h>
+int main(void){
+    char ch;
+    ch = 'X';
+    printf("The character stored id %c", ch);
+    return 0;
+}
